a8d7a4320eacb18e880098016d40a5bbabeb508e
amorcian <morciano.andrea92@gmail.com>
2020-11-30 22:54:06 +0200

    First commit

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "Flake for portmanteau - portfolio manager to end all uncertainties";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    mach-nix = {
+      url = github:DavHau/mach-nix/7266eda5065f509e76cdde66c5ec3f297c56a946; # conda beta
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
+  };
+  outputs = { self, nixpkgs, mach-nix, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = (import nixpkgs { inherit system; config.allowUnsupportedSystem = true; }).pkgs;
+        mach-nix-utils = import mach-nix {
+          inherit pkgs;
+          python = "python3";
+        };
+        requirements = ''
+            pyportfolioopt
+            pytest
+          '';
+      in rec
+      {
+
+        devShell = mach-nix-utils.mkPythonShell {
+          inherit requirements;
+          providers = {
+            alsa-lib = "nixpkgs";
+          };
+        };
+
+        packages.portmanteau = mach-nix-utils.mkPython {
+          inherit requirements;
+        };
+
+        defaultPackage = packages.portmanteau;
+
+      }
+  );
+}
