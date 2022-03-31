92c1370139f671bcdde1e88fdf32a07a386376b0
ae-mo <morciano.andrea92@gmail.com>
2022-03-31 22:04:43 +0300

    Update to python 3.9

--- shell.nix
+++ shell.nix
@@ -1,5 +1,5 @@
 { pkgs ? import <nixpkgs> {} }:
 
 pkgs.mkShell {
-    buildInputs = [ pkgs.python3 pkgs.poetry pkgs.python38Packages.numpy pkgs.python38Packages.pandas-datareader ];
+    buildInputs = [ pkgs.python3 pkgs.poetry pkgs.python39Packages.numpy pkgs.python39Packages.pandas-datareader ];
 }
